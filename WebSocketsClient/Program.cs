5a395c887d564debc1f70bf8d662f4bc3e93aaaf
MITAKE\zed.yang <zed.yang@mitake.com.tw>
2024-06-03 14:43:05 +0800

    update

--- WebSocketsClient/Program.cs
+++ WebSocketsClient/Program.cs
@@ -1,70 +1,144 @@
-﻿using System;
-using System.Net.WebSockets;
+﻿using System.Net.WebSockets;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace WebSocketsClient
 {
     class Program
     {
-        static async Task Main()
+        private const string ServerUri = "ws://127.0.0.1:8080/"; // 替換成你的WebSocket伺服器地址
+        private const int ReconnectIntervalInSeconds = 5;
+
+        private static async Task Main(string[] args)
+        {
+            List<Task> clientTasks = new List<Task>();
+            int clientConut = 15;
+
+            for (int i = 0; i < clientConut; i++)
+            {
+                var count = i + 1;
+                clientTasks.Add(StartClientAsync(count.ToString()));
+            }
+
+            await Task.WhenAll(clientTasks);
+        }
+
+        private static async Task StartClientAsync(string strNumber)
         {
             while (true)
             {
-                await ConnectToServerAsync();
+                using (var client = new ClientWebSocket())
+                {
+                    try
+                    {
+                        client.Options.SetRequestHeader("ClientID", strNumber);
+                        client.Options.SetRequestHeader("ClientGroup", (int.Parse(strNumber) / 2).ToString());
+
+                        await client.ConnectAsync(new Uri(ServerUri), CancellationToken.None);
+
+                        Console.WriteLine($"{strNumber} Connected to the server: {ServerUri}");
+
+                        var receiveTask = ReceiveMessage(client, strNumber);
+                        //var pingTask = SendPingPeriodically(client, strNumber);
+                        //var inputTask = Task.Run(() => HandleConsoleInputAsync(client, strNumber));
+
+                        await Task.WhenAll(receiveTask);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"{strNumber} Error: {ex.Message}");
+                    }
+
+                    Console.WriteLine($"{strNumber} Disconnected. Reconnecting in {ReconnectIntervalInSeconds} seconds...");
+                    await Task.Delay(TimeSpan.FromSeconds(ReconnectIntervalInSeconds));
+                }
             }
         }
 
-        static async Task ConnectToServerAsync()
+        private static async Task ReceiveMessage(ClientWebSocket client, string strNumber)
         {
-            var serverUri = new Uri("ws://127.0.0.1:8080/");
+            var buffer = new byte[1024];
 
-            while (true)
+            while (client.State == WebSocketState.Open)
             {
-                var clientWebSocket = new ClientWebSocket();
-
                 try
                 {
-                    // 連線至伺服器
-                    await clientWebSocket.ConnectAsync(serverUri, CancellationToken.None);
-                    Console.WriteLine($"WebSocket connected to server: {serverUri}");
+                    var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
 
-                    // 傳送一個 "Hi" 訊息
-                    string initialMessage = "Hi";
-                    byte[] initialMessageBytes = Encoding.UTF8.GetBytes(initialMessage);
-                    await clientWebSocket.SendAsync(new ArraySegment<byte>(initialMessageBytes), WebSocketMessageType.Text, true, CancellationToken.None);
-
-                    // 主通信迴圈
-                    while (clientWebSocket.State == WebSocketState.Open)
+                    if (result.MessageType == WebSocketMessageType.Text)
                     {
-                        // 接收伺服器發送的訊息
-                        var buffer = new byte[1024];
-                        var result = await clientWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
-                        if (result.MessageType == WebSocketMessageType.Text)
-                        {
-                            var receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                            Console.WriteLine($"Received message from server: {receivedMessage}");
-                        }
+                        var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                        Console.WriteLine($"{strNumber} Received message: {message}");
+                    }
+                    else if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await client.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
                     }
                 }
                 catch (WebSocketException ex)
                 {
-                    Console.WriteLine($"WebSocket error: {ex.Message}");
-
-                    // 等待 5 秒再重新嘗試連線
-                    await Task.Delay(5000);
-                }
-                finally
-                {
-                    // 關閉 WebSocket 連線
-                    if (clientWebSocket.State == WebSocketState.Open)
-                    {
-                        await clientWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
-                    }
+                    Console.WriteLine($"{strNumber} WebSocket error: {ex.Message}");
+                    break;
                 }
             }
+        }
+
+        private static async Task SendPingPeriodically(ClientWebSocket client, string strNumber)
+        {
+            var maxPingResponseTime = TimeSpan.FromSeconds(30);
+            var lastPingTime = DateTime.Now;
+            int sendCount = 0;
+
+            while (client.State == WebSocketState.Open)
+            {
+                try
+                {
+                    lastPingTime = DateTime.Now;
+
+                    // 發送ping
+                    await client.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("ping")), WebSocketMessageType.Text, true, CancellationToken.None);
+                    Console.WriteLine($"{strNumber} Sent ping." + sendCount.ToString());
+                    sendCount += 1;
+
+                    // 檢查上次ping回來的時間
+                    if ((DateTime.Now - lastPingTime) > maxPingResponseTime)
+                    {
+                        Console.WriteLine($"{strNumber} Ping response time exceeded {maxPingResponseTime.TotalSeconds} seconds. Closing connection.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{strNumber} Ping response time : {(DateTime.Now - lastPingTime).TotalMilliseconds} Milliseconds.");
+                    }
+
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+                }
+                catch (WebSocketException ex)
+                {
+                    Console.WriteLine($"{strNumber} WebSocket error: {ex.Message}");
+                    break;
+                }
+            }
+        }
+
+        private static async Task HandleConsoleInputAsync(ClientWebSocket client, string strNumber)
+        {
+            while (client.State == WebSocketState.Open)
+            {
+                try
+                {
+                    string message = Console.ReadLine();
+                    var buffer = Encoding.UTF8.GetBytes(message);
+
+                    // 發送訊息
+                    await client.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+                catch (WebSocketException ex)
+                {
+                    Console.WriteLine($"{strNumber}  WebSocket error: {ex.Message}");
+                    break;
+                }
+            }
+
         }
     }
 
